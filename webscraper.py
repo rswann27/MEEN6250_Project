b8c6ff9576004a1ac6073785b632fad5f9284e51
Riley Swann <r.swann@sarcos.net>
2022-12-01 20:20:38 -0700

    reformating analysis and adding JSON file to repo

--- webscraper.py
+++ webscraper.py
@@ -3,12 +3,80 @@ import requests
 import json
 
 url = "https://www.worldometers.info/coronavirus/#nav-yesterday2"
-result = requests.get(url).text
-doc = BeautifulSoup(result, 'html.parser')
-tbody = doc.find('table', attrs={'id':"main_table_countries_yesterday2"}).tbody
-countries = tbody.find_all('a',attrs={'class':"mt_a"})
 
-def webscraper(cntry):
+def scrape_country(cntry,url):
+    bool = 1
+
+    # SCRAPE web for data:
+    result = requests.get(url).text
+    doc = BeautifulSoup(result, 'html.parser')
+    tbody = doc.find('table', attrs={'id':"main_table_countries_yesterday2"}).tbody
+    countries = tbody.find_all('a',attrs={'class':"mt_a"})
+
+    # Organize Data for JSON file
+    data = {}
+    for i in range(len(countries)):
+        tr=countries[i].parent.parent
+        tds=tr.find_all('td')
+
+
+        # POPULATION
+        population = tds[14].text
+        population = population.strip()
+        population = int(population.replace(',', ''))
+        data.setdefault(countries[i].text, {})['Population'] = population
+
+        # TOTAL DEATHS
+        Deaths = tds[4].text
+        Deaths = Deaths.strip()
+        if Deaths == "":
+            Deaths = 0
+        else:
+            Deaths = int(Deaths.replace(',', ''))
+        data.setdefault(countries[i].text, {})['Deaths'] = Deaths
+
+        # DEATHS Per MILLION
+        deathPerMil = tds[4].text
+        deathPerMil = deathPerMil.strip()
+        if deathPerMil == "":
+            deathPerMil = 0
+        else:
+            deathPerMil = int(deathPerMil.replace(',', ''))
+        data.setdefault(countries[i].text, {})['Deaths/Million'] = deathPerMil
+
+        # NEW DEATHS
+        newDeaths = tds[5].text
+        newDeaths = newDeaths.strip()
+        if newDeaths == "":
+            newDeaths = 0
+        else:
+            newDeaths = int(newDeaths.replace(',', ''))
+        data.setdefault(countries[i].text, {})['New Deaths'] = newDeaths
+
+        # NEW DEATH per MILLION
+        data.setdefault(countries[i].text, {})['New Deaths/Million'] = round(newDeaths/(population/100000),3)
+
+
+        # RETURN data associated with given country
+        if cntry == countries[i].text:
+            print(cntry,":" )
+            print("   Daily Death Rate = ", data[cntry]['New Deaths'])
+            print("   Daily Death Rate Per Million = ", data[cntry]['New Deaths/Million'])
+            print("   Cumulative Death Rate = ", data[cntry]['Deaths'])
+            print("   Cumulative Death Rate Per Million = ", data[cntry]['Deaths/Million'])
+            # We found the given country
+            bool = 0
+
+    # Did we find the given country?
+    if bool == 1:
+        # No
+        print('Country Not in Data Set')
+
+    # SAVE Dictionary in JSON file
+    with open("COVID.json", "w") as outfile:
+        json.dump(data, outfile)
+"""
+    # Return specific country
     for c in countries:
         if c.text == cntry:
             tds = c.parent.parent.find_all('td')
@@ -21,50 +89,14 @@ def webscraper(cntry):
             print(deathsPerMil)
             print((newdeaths))
             print(newDeathsPerMil)
-            
-webscraper('Brazil')
-
-data = {}
-for i in range(len(countries)):
-    tr=countries[i].parent.parent
-    tds=tr.find_all('td')
-
-    # TOTAL DEATHS
-    Deaths = tds[4].text
-    Deaths = Deaths.strip()
-    data.setdefault(countries[i].text, {})['Deaths'] = Deaths
-
-    # DEATHS Per MILLION
-    deathPerMil = tds[4].text
-    deathPerMil = deathPerMil.strip()
-    data.setdefault(countries[i].text, {})['Deaths/Million'] = deathPerMil
-
-    # TOTAL CASES
-    totalCases = tds[13].text
-    totalCases = totalCases.strip()
-    data.setdefault(countries[i].text, {})['Total Cases'] = totalCases
-
-    # NEW CASES
-    casePerMil = tds[10].text
-    casePerMil = casePerMil.strip()
-    data.setdefault(countries[i].text, {})['Caese/Mil'] = casePerMil
-
-    # CASES Per MILLION
-    newCases = tds[3].text
-    newCases = newCases.strip()
-    data.setdefault(countries[i].text, {})['New Cases'] = newCases
-
-
-    # POPULATION
-    population = tds[14].text
-    population = population.strip()
-    data.setdefault(countries[i].text, {})['Population'] = population
-
-with open("COVID.json", "w") as outfile:
-    json.dump(data, outfile)
-
-
+"""            
+scrape_country('Brazil',url)
 """
+
+
+
+
+
 
 #Print its total deaths
 tr=countries[0].parent.parent
