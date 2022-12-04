a9ee31c1ed6a1737c4ddec312c596e7e80b5bcd6
Riley Swann <r.swann@sarcos.net>
2022-12-03 21:08:31 -0700

    adding second website option

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # MEEN6250_Project
 In this Python repo, we designed and implemented an interactive dashboard to support the visualization of coronavirus-related data. To accomplish this, we use Beautiful Soup for Data Scraping, Bokeh for displaying information, and Github for delivering the project.
 
-'ScrapeWebsite.py' contains the function 'scrape_country' which takes two inputs: the desired country and a default arguement of a COVID data website url. The script is currently set up to scrape the following website:
+'ScrapeWebsite.py' contains the function 'scrape_country' which takes two inputs: the desired country and a default arguement of a COVID data website url set to Wolrd-o-Meter. The script is currently set up to scrape the following websites:
 
 https://www.worldometers.info/coronavirus/#nav-yesterday2
+https://www.nytimes.com/interactive/2021/world/covid-cases.html
 
-The function parses through all the counties' data and stores their Population, Total Deaths, Deaths Per Million, New Daily Deaths, and New Daily Deaths Per Million to a multi-dimensional dictionary sorted by country. The dictionary is then saved in a json file to the current folder location under 'COVID_*CurrentDate*.json'. If the desired country is found in the data set, the function will return that country's saved data to the termial, for example:
+The function parses through all the counties' data and creates a multi-dimensional dictionary sorted by country and store the following if parsing through World-o-Meter: Population, Total Deaths, Deaths Per Million, New Daily Deaths, and New Daily Deaths Per Million and if parsing through NYTimes: New Daily Deaths, and New Daily Deaths Per Million. The dictionary is then saved in a json file to the current folder location under 'COVID_*CurrentDate*.json'. If the desired country is found in the data set, the function will return that country's saved data to the termial, for example:
 
    Brazil :
    
@@ -19,7 +20,8 @@ The function parses through all the counties' data and stores their Population, 
     
 If the country is not found in the data set, the funciton will return "Country not found". Whether or not the given country is found, the function will save all the countries' data to the json file.
 
-To changed the desired country (it's currently set to 'Brazil') change the variable 'desiredCountry' on line 6 to your desired country and run the Python code.
+To change the desired country (it's currently set to 'Brazil') change the variable 'desiredCountry' on line 6 to the desired country.
+To toggle the desired url from World-o-Meter to NYTime, uncomment line 135 and comment out line 136.
  
 See documentation for Beautiful Soup installation: 
 https://www.crummy.com/software/BeautifulSoup/bs4/doc/
--- ScrapeWebsite.py
+++ ScrapeWebsite.py
@@ -8,65 +8,129 @@ desiredCountry = 'Brazil'
 def scrape_country(cntry,url ="https://www.worldometers.info/coronavirus/#nav-yesterday2"):
     bool = 1
 
+    # CHECK for valid url
+    if url != "https://www.worldometers.info/coronavirus/#nav-yesterday2" or url !="https://www.nytimes.com/interactive/2021/world/covid-cases.html":
+        print("Not valid webset - setting to default")
+        url ="https://www.worldometers.info/coronavirus/#nav-yesterday2"
+
     # SCRAPE web for data:
     result = requests.get(url).text
     doc = BeautifulSoup(result, 'html.parser')
-    tbody = doc.find('table', attrs={'id':"main_table_countries_yesterday2"}).tbody
-    countries = tbody.find_all('a',attrs={'class':"mt_a"})
+    if url == "https://www.nytimes.com/interactive/2021/world/covid-cases.html":
+        tbody = doc.find('tbody', attrs={'class':"children"})
+        cities = tbody.find_all('tr')
 
-    # Organize Data for JSON file
-    data = {}
-    for i in range(len(countries)):
-        tr=countries[i].parent.parent
-        tds=tr.find_all('td')
+        data = {}
+        for i in range(len(cities)):
+            tds = cities[i].find_all('td')
+
+            name = tds[0].text
+            name = name.replace('›', '')
+            name = name.strip()
+
+            ''''
+            ## NEW CASES
+            dailyCases = tds[1].text
+            dailyCases = dailyCases.strip()
+            dailyCases = int(dailyCases.replace(',', ''))
+            data.setdefault(name, {})['Daily Case Rate'] = dailyCases
+
+            # NEW CASES Per MILLION
+            if tds[2].text == '—':
+                dailyCaseperMil = 0.0
+            else:
+                dailyCaseperMil = tds[2].text
+                dailyCaseperMil = dailyCaseperMil.strip()
+            data.setdefault(name, {})['Daily Case Rate/Million'] = dailyCaseperMil
+            '''
+
+            # NEW DEATHS
+            if tds[4].text == '—':
+                newDeaths = 0.0
+            else:
+                newDeaths = tds[4].text
+                newDeaths = newDeaths.strip()
+                newDeaths = newDeaths.replace(',', '')
+            data.setdefault(name, {})['New Deaths'] = newDeaths
+
+            # NEW DEATHS Per MILLION
+            if tds[5].text == '—':
+                deathsPerMil = 0.0
+            else:
+                deathsPerMil = round(float(tds[5].text)/10, 2)
+            data.setdefault(name, {})['New Deaths/Million'] = newDeaths
+
+            # RETURN data associated with given country
+            if cntry == name:
+                print("\n\n\n",cntry,":" )
+                '''
+                print("   Daily Case Rate = ",data.get(cntry,{}).get('Daily Case Rate'))
+                print("   Daily Case Rate Per Million = ",data.get(cntry,{}).get('Daily Case Rate/Million'))
+                '''
+                print("   Daily Death Rate = ", data.get(cntry,{}).get('New Deaths'))
+                print("   Daily Death Rate Per Million = ", data.get(cntry,{}).get('New Deaths/Million'), "\n\n\n")
+                # We found the given country
+                bool = 0
+            
+
+    else: 
+        tbody = doc.find('table', attrs={'id':"main_table_countries_yesterday2"}).tbody
+        countries = tbody.find_all('a',attrs={'class':"mt_a"})
+
+        # Organize Data for JSON file
+        data = {}
+        for i in range(len(countries)):
+            tr=countries[i].parent.parent
+            tds=tr.find_all('td')
 
 
-        # POPULATION
-        population = tds[14].text
-        population = population.strip()
-        population = int(population.replace(',', ''))
-        data.setdefault(countries[i].text, {})['Population'] = population
+            # POPULATION
+            population = tds[14].text
+            population = population.strip()
+            population = int(population.replace(',', ''))
+            data.setdefault(countries[i].text, {})['Population'] = population
 
-        # TOTAL DEATHS
-        Deaths = tds[4].text
-        Deaths = Deaths.strip()
-        if Deaths == "":
-            Deaths = 0
-        else:
-            Deaths = int(Deaths.replace(',', ''))
-        data.setdefault(countries[i].text, {})['Deaths'] = Deaths
+            # TOTAL DEATHS
+            Deaths = tds[4].text
+            Deaths = Deaths.strip()
+            if Deaths == "":
+                Deaths = 0
+            else:
+                Deaths = int(Deaths.replace(',', ''))
+            data.setdefault(countries[i].text, {})['Deaths'] = Deaths
 
-        # DEATHS Per MILLION
-        deathPerMil = tds[4].text
-        deathPerMil = deathPerMil.strip()
-        if deathPerMil == "":
-            deathPerMil = 0
-        else:
-            deathPerMil = int(deathPerMil.replace(',', ''))
-        data.setdefault(countries[i].text, {})['Deaths/Million'] = deathPerMil
+            # DEATHS Per MILLION
+            deathPerMil = tds[4].text
+            deathPerMil = deathPerMil.strip()
+            if deathPerMil == "":
+                deathPerMil = 0
+            else:
+                deathPerMil = int(deathPerMil.replace(',', ''))
+            data.setdefault(countries[i].text, {})['Deaths/Million'] = deathPerMil
 
-        # NEW DEATHS
-        newDeaths = tds[5].text
-        newDeaths = newDeaths.strip()
-        if newDeaths == "":
-            newDeaths = 0
-        else:
-            newDeaths = int(newDeaths.replace(',', ''))
-        data.setdefault(countries[i].text, {})['New Deaths'] = newDeaths
+            # NEW DEATHS
+            newDeaths = tds[5].text
+            newDeaths = newDeaths.strip()
+            if newDeaths == "":
+                newDeaths = 0
+            else:
+                newDeaths = int(newDeaths.replace(',', ''))
+            data.setdefault(countries[i].text, {})['New Deaths'] = newDeaths
 
-        # NEW DEATH per MILLION
-        data.setdefault(countries[i].text, {})['New Deaths/Million'] = round(newDeaths/(population/100000),3)
+            # NEW DEATH per MILLION
+            data.setdefault(countries[i].text, {})['New Deaths/Million'] = round(newDeaths/(population/100000),3)
 
 
-        # RETURN data associated with given country
-        if cntry == countries[i].text:
-            print("\n\n\n",cntry,":" )
-            print("   Daily Death Rate = ", data.get(cntry,{}).get('New Deaths'))
-            print("   Daily Death Rate Per Million = ", data.get(cntry,{}).get('New Deaths/Million'))
-            print("   Cumulative Death Rate = ", data.get(cntry,{}).get('Deaths'))
-            print("   Cumulative Death Rate Per Million = ", data.get(cntry,{}).get('Deaths/Million'),"\n\n\n")
-            # We found the given country
-            bool = 0
+            # RETURN data associated with given country
+            if cntry == countries[i].text:
+                print("\n\n\n",cntry,":" )
+                print("   Daily Death Rate = ", data.get(cntry,{}).get('New Deaths'))
+                print("   Daily Death Rate Per Million = ", data.get(cntry,{}).get('New Deaths/Million'))
+                print("   Cumulative Death Rate = ", data.get(cntry,{}).get('Deaths'))
+                print("   Cumulative Death Rate Per Million = ", data.get(cntry,{}).get('Deaths/Million'),"\n\n\n")
+                # We found the given country
+                bool = 0
+
 
     # Did we find the given country?
     if bool == 1:
@@ -78,7 +142,9 @@ def scrape_country(cntry,url ="https://www.worldometers.info/coronavirus/#nav-ye
     with open(saveStr, "w") as outfile:
         json.dump(data, outfile)
            
+#scrape_country(desiredCountry, "https://www.nytimes.com/interactive/2021/world/covid-cases.html")
 scrape_country(desiredCountry)
+#scrape_country(desiredCountry,"http://www.thisisnotvalid.com")
 """
 
 # open json file
